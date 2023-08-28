ab7665c7b0364d1bea19a49698e9a2a19db8cbad
Göktay İncekara <goktayincekara@gmail.com>
2023-08-28 11:31:54 +0300

    Add auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!!!");
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!");
