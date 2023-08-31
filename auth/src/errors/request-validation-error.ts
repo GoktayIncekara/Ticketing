6edda791a06d5294252a1f21d9e161836fa39902
Göktay İncekara <goktayincekara@gmail.com>
2023-08-31 16:11:29 +0300

    Add database management and modeling

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -4,7 +4,7 @@ import { CustomError } from "./custom-error";
 export class RequestValidationError extends CustomError {
   statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super("Invalid request parameters");
+    super("Invalid request parameters"); //Just for logging purposes, it wont be showed to the user
 
     //Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
@@ -36,6 +38,17 @@ app.all("*", async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth"); //the name of the service to connect to mongodb. And it will create a db automatically called auth (since we wrote it like that here)
+    console.log("connected");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!");
+  });
+};
+
+start();
