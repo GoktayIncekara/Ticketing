3f899e171d9e1c1ba42b167c9aeb96a2e8f15648
Göktay İncekara <goktayincekara@gmail.com>
2023-09-06 13:53:58 +0300

    Move the getInitialProps and add reusable API client

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,29 @@
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css"; //We created this component to be able to add global css or to add a component to be visible in each page
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header! {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+//context === { Component, ctx: { req, res }} in Custom App Component
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data, // currentUser: currentUserValue
+  };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,24 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 //If we ever want to fetch some data with next.js during the server side rendering process we will use this
 //this function will be executed in the server
 //I can reach this functions return values from the component props
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === "undefined") {
-    //we are on the server
-    //http://SERVICENAME.NAMESPACE.svc.cluster.local
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: {
-          Host: "ticketing.dev",
-        },
-      }
-    );
+//conext === { req, res } in Page Component
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
 
-    return data;
-  } else {
-    //we are on the browser
-    const response = await axios.get("/api/users/currentuser").catch((err) => {
-      console.log(err.message);
-    });
-    return response.data;
-  }
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
 };
 
 export default LandingPage;
