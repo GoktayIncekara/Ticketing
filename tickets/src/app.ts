076cd5ca86cdcd9e96963f1cb9222e50bd207f3b
Göktay İncekara <goktayincekara@gmail.com>
2023-09-08 14:29:43 +0300

    Add ticketing service

---
+++ tickets/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError } from "@ticketsgi/common";
+
+const app = express();
+app.set("trust proxy", true); //To make sure that express is aware that is behind the proxy of ingress-ngx and make sure it trust the traffic
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+); //secure:true means that cookies are only going to be shared when someone is making a request to our server over an https connection
+
+app.all("*", async (req, res) => {
+  //Bcs of express-async-errors we would be able to get the async keyword to work in express
+  throw new NotFoundError();
+});
+
+/*
+app.all("*", () => {
+  throw new NotFoundError();
+});
+*/
+
+/*
+app.all("*", async (req, res, next) => {
+  next(new NotFoundError());
+});
+*/
+
+app.use(errorHandler);
+
+export { app };
