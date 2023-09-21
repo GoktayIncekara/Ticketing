5b24a7e742215ed7687f9df05de97c46b9585afc
Göktay İncekara <goktayincekara@gmail.com>
2023-09-21 14:34:51 +0300

    Final version of the NAST test project

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,9 +10,21 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("orders-service");
+
   const subscription = stan.subscribe(
     "ticket:created",
-    "orders-service-queue-group"
+    "orders-service-queue-group", //Because of the queue group name, if the service gets disconnected it will continue to use setDurableName in above to keep track of the received events.
+    options
   );
 
   subscription.on("message", (msg: Message) => {
@@ -20,5 +32,10 @@ stan.on("connect", () => {
     if (typeof data === "string") {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
