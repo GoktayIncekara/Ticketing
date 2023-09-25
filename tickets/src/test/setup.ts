fa166d4c57d22e1393f3772d41ee71d30d957137
Göktay İncekara <goktayincekara@gmail.com>
2023-09-25 10:00:38 +0300

    Providing mock implementation for NATS

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -8,6 +8,8 @@ declare global {
   var signup: () => string[];
 }
 
+jest.mock("../nats-wrapper");
+
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "asdfasd";
@@ -18,6 +20,7 @@ beforeAll(async () => {
 }, 10000);
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
   for (let collection of collections) {
     await collection.deleteMany({});
