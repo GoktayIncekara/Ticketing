65b1e028828f3b94e5d79d457df44fa860155a88
Göktay İncekara <goktayincekara@gmail.com>
2023-09-25 15:38:28 +0300

    Add the order service, scaffold it and add route handlers

---
+++ orders/src/app.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import "express-async-errors";
+
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError, currentUser } from "@ticketsgi/common";
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+
+const app = express();
+app.set("trust proxy", true); //To make sure that express is aware that is behind the proxy of ingress-ngx and make sure it trust the traffic
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+); //secure:true means that cookies are only going to be shared when someone is making a request to our server over an https connection
+app.use(currentUser);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+
+app.all("*", async (req, res) => {
+  //Bcs of express-async-errors we would be able to get the async keyword to work in express
+  throw new NotFoundError();
+});
+
+/*
+app.all("*", () => {
+  throw new NotFoundError();
+});
+*/
+
+/*
+app.all("*", async (req, res, next) => {
+  next(new NotFoundError());
+});
+*/
+
+app.use(errorHandler);
+
+export { app };
