74abdff23ec4890666dcf7a3aa6fe8a0928a8d0c
Göktay İncekara <goktayincekara@gmail.com>
2023-09-26 08:59:02 +0300

    Add finding reserved tickets and order expiration times

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Order, OrderStatus } from "./order";
 
 interface TicketAttrs {
   title: string;
@@ -8,6 +9,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -37,7 +39,24 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  //statics object is how we add a new method directly to the ticket model itself
   return new Ticket(attrs);
+};
+ticketSchema.methods.isReserved = async function () {
+  //to add a new method to the ticket doc
+  // this === the ticket document that we just called "isReserved" on
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return !!existingOrder; //if its null => true => false  //if its true => false => true
 };
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from "express";
-import { NotFoundError, requireAuth, validateRequest } from "@ticketsgi/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@ticketsgi/common";
 import { body } from "express-validator";
 import mongoose from "mongoose";
 import { Ticket } from "../models/ticket";
 import { Order } from "../models/order";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -29,7 +37,24 @@ router.post(
       throw new NotFoundError();
     }
 
-    res.send({});
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    res.status(201).send(order);
   }
 );
 
