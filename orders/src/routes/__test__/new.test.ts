72202f39a784ef48365f68046b166f7c9ba468f1
Göktay İncekara <goktayincekara@gmail.com>
2023-09-26 20:14:56 +0300

    Create order service events, publishers and tests for event publishing

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import { app } from "../../app";
 import mongoose from "mongoose";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("should return an error if the ticket does not exist", async () => {
   const ticketId = new mongoose.Types.ObjectId();
@@ -49,4 +50,17 @@ it("should reserve a ticket", async () => {
     .expect(201);
 });
 
-it.todo("should emit an order created event");
+it("should emit an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signup())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
