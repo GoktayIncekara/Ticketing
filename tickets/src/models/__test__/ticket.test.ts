72cec02ed3ca48da4d9757a7f615ab36fb13ff36
Göktay İncekara <goktayincekara@gmail.com>
2023-09-27 13:54:43 +0300

    Including versions in events and abstracted query method

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -25,3 +25,20 @@ it("should implement optimistic concurrency control", async () => {
 
   throw new Error("Should not reach this point");
 });
+
+it("should increment the version number on multiple saves", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+    userId: "123",
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
